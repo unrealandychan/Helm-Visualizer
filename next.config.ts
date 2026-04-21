afcf5a9ab8bea61adde85058e6e4d8d8651dfdaf
Eddie Chan <eddie.chan@connect.ust.hk>
2026-04-21 14:11:07 +0800

    fix: resolve path issue

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["tar"],
@@ -6,6 +7,9 @@ const nextConfig: NextConfig = {
     serverActions: {
       bodySizeLimit: "50mb",
     },
+  },
+  turbopack: {
+    root: path.resolve(__dirname),
   },
 };
 
